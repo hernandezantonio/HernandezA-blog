e031a7da1e599539703ae65b59b71266e61c1008
antonio hernandez <defygaming99@gmail.com>
2015-01-29 11:15:05 -0800

    no message

---
+++ controller/create-db.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once(__DIR__  ."/../model/database.php");
+
+$connection = new mysqli($host, $username, $password ); 
+
+if($connection-> connect_error){
+    die("Error: ". $connection-> connect_error);
+}
+
+$exist = $connection->select_db($database);
+
+if(!$exist){
+   $query = $connection-> query("CREATE DATABASE $database ");
+  
+           if($query){
+               echo "successfuly created database: ". $database;  
+            }
+           }
+           else{
+               echo "database already exist."; 
+           }
+
+
+$connection->close();  
