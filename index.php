d0dbfb27ba2c20184ebbdf17011d6edc189070dd
antonio hernandez <defygaming99@gmail.com>
2015-02-26 11:09:58 -0800

    final

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 
+
+<!--navbar code-->
+
+   <nav>
+                <a id="current-link" href="index.php">Home</a>
+                <a href="login.php">Login</a>  
+                <a href="register.php">sign up
+              </a>
+            </nav>
+            
+
+
+<!--this lets everything show up when you first run the project-->
     <?php
        require_once (__DIR__ . "/controller/login-verify.php");
        require_once (__DIR__."/view/header.php"); 
--- register.php
+++ register.php
@@ -1,3 +1,15 @@
+
+
+
+
+   <nav>
+                <a id="current-link" href="index.php">Home</a>
+                <a href="login.php">Login</a>  
+                <a href="register.php">sign up
+              </a>
+            </nav>
+           
+
 <?php
 require_once(__DIR__ . "/view/header.php");
 require_once(__DIR__ . "/view/register-form.php");
